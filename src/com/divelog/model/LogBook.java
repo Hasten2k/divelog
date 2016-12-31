8d9bddc07468816e03bd20482ec85c10ccd772fc
Carl Stalhem <carl@stalhem.se>
2016-12-31 12:14:46 +0100

    Initial version

---
+++ src/com/divelog/model/LogBook.java
@@ -0,0 +1,11 @@
+package com.divelog.model;
+
+import java.util.ArrayList;
+
+public class LogBook {
+  private ArrayList<Dive> mLoggedDives;
+
+  public LogBook() {
+    mLoggedDives = new ArrayList<>();
+  }
+}
