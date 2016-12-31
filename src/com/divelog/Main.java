0bcec961be362c656c159b4d7b34f789f3b7f67a
Carl Stalhem <carl@stalhem.se>
2016-12-31 13:06:21 +0100

    Updated test content.

--- src/com/divelog/Main.java
+++ src/com/divelog/Main.java
@@ -12,10 +12,14 @@ public class Main {
 	// write your code here
       User carl = new User("cstalhem", "Carl","Stalhem", "carl@stalhem.se");
 
-      DiveSite elphinstone = new DiveSite("Elphinstone", "Egypt");
+      DiveSite elphinstone = new DiveSite("Elphinstone Reef", "Egypt");
 
       Dive dive = new Dive(elphinstone, new Date(1480329953000L), 40, 35);
 
       carl.getLogBook().addDive(dive);
+
+      String log = carl.getLogBook().toString();
+
+      System.out.println(log);
     }
 }
