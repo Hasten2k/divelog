f66908031590ff80afb65d806ceefe6f600f90a2
Carl Stalhem <carl@stalhem.se>
2016-12-31 11:51:55 +0100

    Refactored code to take new /model dir into account

--- src/com/divelog/Main.java
+++ src/com/divelog/Main.java
@@ -1,17 +1,23 @@
 package com.divelog;
 
+import com.divelog.model.Dive;
+
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-      Dive dive = new Dive("Elphinstone Reef", new Date(20161128), 40, 35);
+      Dive dive = new Dive("Elphinstone Reef", new Date(1480329953000L), 40, 35);
       String site = dive.getDiveSite();
+      String date = dive.getDiveDate().toString();
       int time = dive.getDiveTime();
       int depth = dive.getMaxDepth();
 
-      System.out.printf("Your last dive was at %s, lasted for %d minutes and your max depth was " +
-          "%d meters.", site, time, depth);
+      System.out.printf("Your last dive was at %s, on %s, lasted for %d minutes and your max " +
+          "depth" +
+          " was " +
+          "%d meters.%n", site, date, time, depth);
+      System.out.println(dive.toString());
     }
 }
