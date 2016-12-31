38b54465d1c3cdd0362689eb5bdb8a6b7fdbfe03
Carl Stalhem <carl@stalhem.se>
2016-12-31 12:43:43 +0100

    Added getters and Setters

--- src/com/divelog/model/User.java
+++ src/com/divelog/model/User.java
@@ -16,4 +16,52 @@ public class User {
     mEmail = email;
     mLogBook = new LogBook();
   }
+
+  public void setFirstName( String firstName ) {
+    mFirstName = firstName;
+  }
+
+  public void setLastName( String lastName ) {
+    mLastName = lastName;
+  }
+
+  public void setEmail( String email ) {
+    mEmail = email;
+  }
+
+  public void setNumberOfDives( int numberOfDives ) {
+    mNumberOfDives = numberOfDives;
+  }
+
+  public void setDiveSitesVisited( int diveSitesVisited ) {
+    mDiveSitesVisited = diveSitesVisited;
+  }
+
+  public String getUserName() {
+    return mUserName;
+  }
+
+  public String getFirstName() {
+    return mFirstName;
+  }
+
+  public String getLastName() {
+    return mLastName;
+  }
+
+  public String getEmail() {
+    return mEmail;
+  }
+
+  public LogBook getLogBook() {
+    return mLogBook;
+  }
+
+  public int getNumberOfDives() {
+    return mNumberOfDives;
+  }
+
+  public int getDiveSitesVisited() {
+    return mDiveSitesVisited;
+  }
 }
