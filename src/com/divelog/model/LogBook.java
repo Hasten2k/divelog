9f3aafd5cd2b2bc4fbf6280a790f3b8956729fd9
Carl Stalhem <carl@stalhem.se>
2016-12-31 12:29:04 +0100

    Added addDive method.

--- src/com/divelog/model/LogBook.java
+++ src/com/divelog/model/LogBook.java
@@ -8,4 +8,11 @@ public class LogBook {
   public LogBook() {
     mLoggedDives = new ArrayList<>();
   }
+
+  public void addDive(Dive dive) {
+    mLoggedDives.add(dive);
+    int diveNumber = mLoggedDives.size();
+    dive.setDiveNumber(diveNumber);
+    System.out.printf("Dive was added! You have made %d dives. Good job!", diveNumber);
+  }
 }
