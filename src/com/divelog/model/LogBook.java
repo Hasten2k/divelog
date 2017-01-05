93f1ee53c13954c11e131e01891f2cdfc150dbfe
Carl Stalhem <carl@stalhem.se>
2017-01-05 22:17:40 +0100

    Add export method to Logbook. Comma separated values.

--- src/com/divelog/model/LogBook.java
+++ src/com/divelog/model/LogBook.java
@@ -1,5 +1,8 @@
 package com.divelog.model;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class LogBook {
@@ -12,6 +15,35 @@ public class LogBook {
   @Override
   public String toString() {
     return "Your LogBook contains " + getNumberOfDives() + " number of dives.";
+  }
+
+  public void exportLogBookTo(String fileName) {
+    try (
+        FileOutputStream fos = new FileOutputStream(fileName);
+        PrintWriter writer = new PrintWriter(fos)
+    ) {
+      for ( Dive dive : mLoggedDives ) {
+        // print out all dive data to a file.
+        writer.printf("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s%n",
+            dive.getDiveNumber(),
+            dive.getDiveDate(),
+            dive.getDiveSite(),
+            dive.getDiveTime(),
+            dive.getMaxDepth(),
+            dive.getBottomTemp(),
+            dive.getVisibility(),
+            dive.getWaterType(),
+            dive.getWeightsUsed(),
+            dive.getGasType(),
+            dive.getPressureIn(),
+            dive.getPressureOut(),
+            dive.getComments());
+      }
+
+    } catch ( IOException ioe ) {
+      System.out.printf("Problem saving %s.%n", fileName);
+      ioe.printStackTrace();
+    }
   }
 
   public void addDive( Dive dive ) {
